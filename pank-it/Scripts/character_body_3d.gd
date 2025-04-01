39bc33e14ead7a7aa12d047949f8e52d6c39839d
Cameron Hajek <cameronisaachajek@outlook.com>
2025-04-01 14:45:44 -0600

    Initial commit.

---
+++ pank-it/Scripts/character_body_3d.gd
@@ -0,0 +1,41 @@
+extends CharacterBody3D
+
+
+@onready var score_lable = $Label
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+var score = 0
+
+
+func _ready():
+	score_lable.text = "Score: %s" % score
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+func _input(event):
+	if Input.is_action_pressed("honk_horn"):
+		print("HONK")
+		score += 1
+		print(score)
